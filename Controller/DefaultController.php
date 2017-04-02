4b169e307a2d4bcd92d6551ac790f7f2c5ac54db
pivchenberg <pivchenberg@gmail.com>
2017-04-02 23:34:25 +0300

    added draft entities

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -2,6 +2,8 @@
 
 namespace Pivchenberg\XenaBundle\Controller;
 
+use Pivchenberg\XenaBundle\Entity\ArticleNode;
+use Pivchenberg\XenaBundle\Entity\ProductNode;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 
@@ -12,6 +14,31 @@ class DefaultController extends Controller
      */
     public function indexAction()
     {
+        $em = $this->getDoctrine()->getManager();
+//        $food = new ProductNode();
+//        $food->setTitle('Food');
+//
+//        $fruits = new ProductNode();
+//        $fruits->setTitle('Fruits');
+//        $fruits->setParent($food);
+//
+//        $vegetables = new ProductNode();
+//        $vegetables->setTitle('Vegetables');
+//        $vegetables->setParent($food);
+//
+//        $carrots = new ArticleNode();
+//        $carrots->setTitle('Carrots');
+//        $carrots->setParent($vegetables);
+//
+//        $em->persist($food);
+//        $em->persist($fruits);
+//        $em->persist($vegetables);
+//        $em->persist($carrots);
+//        $em->flush();
+
+
+        ($em->getRepository('XenaBundle:BaseNode')->findAll(['id' => 1]));
+        //die();
         return $this->render('XenaBundle:Default:index.html.twig');
     }
 }
